0fe5011472c188d9b7c842f980fc7d7829ba2845
flakolefluk <flakolefluk@gmail.com>
2018-01-28 15:26:37 -0300

    test: Changes to use promises
    Test to allow new use of promises instead of callbacks
    
    BREAKING CHANGE: Not compatible with previous versions

--- test/test.js
+++ test/test.js
@@ -1,88 +1,133 @@
 var rut = require('../index.js')
-// var should = require('chai').should()
 var expect = require('chai').expect
-describe('RUT verifier', function () {
-  it('invalid string returns error', function () {
-    rut.verify('sdfwer', function (err, data) {
-      expect(data).to.be.null
-      expect(err).to.equal('Input contains invalid characters')
-    })
+
+
+describe('RUT verifier', () => {
+  it('invalid string returns error', done => {
+    rut.verify('sdfwer')
+      .then(data => {
+        done(data)
+      })
+      .catch(e => {
+        expect(e.message)
+          .to.equal('Input contains invalid characters')
+        done();
+      })
+      .catch(e => done(e))
   })
 
-  it('invalid character at beginning returns error', function () {
-    rut.verify('a93847362', function (err, data) {
-      expect(data).to.be.null
-      expect(err).to.equal('Input contains invalid characters')
-    })
+  it('invalid character at beginning returns error', done => {
+    rut.verify('a93847362')
+      .then(data =>
+        done(data))
+      .catch(e => {
+        expect(e.message)
+          .to.equal('Input contains invalid characters')
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('invalid character at ending returns error', function () {
-    rut.verify('93847362a ', function (err, data) {
-      expect(data).to.be.null
-      expect(err).to.equal('Input contains invalid characters')
-    })
+  it('invalid character at ending returns error', done => {
+    rut.verify('93847362a')
+
+      .then(data => done(data))
+      .catch(e => {
+        expect(e.message)
+          .to.equal('Input contains invalid characters')
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('invalid character at any position returns error', function () {
-    rut.verify('9384a7362 ', function (err, data) {
-      expect(data).to.be.null
-      expect(err).to.equal('Input contains invalid characters')
-    })
+  it('invalid character at any position returns error', done => {
+    rut.verify('9384a7362')
+      .then(data => done(data))
+      .catch(e => {
+        expect(e.message)
+          .to.equal('Input contains invalid characters')
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('10 characters RUT number is valid', function () {
-    rut.verify('147837488', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.true
-    })
+  it('10 characters RUT number is valid', done => {
+    rut.verify('147837488')
+      .then(data => {
+        expect(data)
+          .to.be.true
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('10 characters RUT number is invalid', function () {
-    rut.verify('147837483', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.false
-    })
+  it('10 characters RUT number is invalid', done => {
+    rut.verify('147837483')
+      .then(data => {
+        expect(data)
+          .to.be.false
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('10 characters RUT number with letter k is valid', function () {
-    rut.verify('14853568k', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.true
-    })
+  it('10 characters RUT number with letter k is valid', done => {
+    rut.verify('14853568k')
+      .then(data => {
+        expect(data)
+          .to.be.true
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('10 characters RUT number with letter k number is invalid', function () {
-    rut.verify('14853567k', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.false
-    })
+  it('10 characters RUT number with letter k number is invalid', done => {
+    rut.verify('14853567k')
+      .then(data => {
+        expect(data)
+          .to.be.false
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('9 characters RUT number is valid', function () {
-    rut.verify('98465340', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.true
-    })
+  it('9 characters RUT number is valid', done => {
+    rut.verify('98465340')
+      .then(data => {
+        expect(data)
+          .to.be.true
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('9 characters RUT number is invalid', function () {
-    rut.verify('98465341', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.false
-    })
+  it('9 characters RUT number is invalid', done => {
+    rut.verify('98465341')
+      .then(data => {
+        expect(data)
+          .to.be.false
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('9 characters RUT with letter k number is valid', function () {
-    rut.verify('4853578k', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.true
-    })
+  it('9 characters RUT with letter k number is valid', done => {
+    rut.verify('4853578k')
+      .then(data => {
+        expect(data)
+          .to.be.true
+        done()
+      })
+      .catch(e => done(e))
   })
 
-  it('9 characters RUT number with letter k is invalid', function () {
-    rut.verify('4853579k', function (err, data) {
-      expect(err).to.be.null
-      expect(data).to.be.false
-    })
+  it('9 characters RUT number with letter k is invalid', done => {
+    rut.verify('4853579k')
+      .then(data => {
+        expect(data)
+          .to.be.false
+        done()
+      })
+      .catch(e => done(e))
   })
 })
